4a5d9a6cac8fc6747a57d6066bf8e7e8fd1b4e31
kiran <kirantyga@gmail.com>
2020-05-29 10:57:40 +0530

    Commit#2

--- src/DuplicatesinanArray.java
+++ src/DuplicatesinanArray.java
@@ -8,35 +8,35 @@ public class DuplicatesinanArray {
 
 	public static void main(String[] args) {
 
-		/*
-		 * String names[]= {"Kiran","Smith","Kiran","Dhoni","Dhoni"};
-		 * HashSet<String>resultset=new HashSet<>(); for(String name: names) {
-		 * if(resultset.add(name)==false) { System.out.println(name); } }
-		 */
+		String names[] = { "Kiran", "Smith", "Kiran", "Dhoni", "Dhoni" };
+		HashSet<String> resultset = new HashSet<>();
+		for (String name : names) {
+			if (resultset.add(name) == false) {
+				System.out.println(name);
+			}
+		}
 
 		/*
 		 * List<String> strings = new ArrayList<String>(); strings.add("Kiran");
 		 * strings.add("Smith"); strings.add("Kiran"); strings.add("Dhoni ");
 		 */
-		
-		
-		String sentense= "Hello there my name is kiran kumar kiran is having 4 years of experience java is using by many in the world these days";
-		
-		String strings[]=sentense.split(" ");
 
-		Map<String, Integer> resultMap = new HashMap<String, Integer>();
-
-		for (String str : strings) {
-		    if (resultMap.containsKey(str)) {
-		    	resultMap.put(str, resultMap.get(str) + 1);
-		    } else {
-		    	resultMap.put(str, 1);
-		    }
-		}
-
-		for (Map.Entry<String, Integer> entry : resultMap.entrySet()) {
-		    System.out.println(entry.getKey() + " = " + entry.getValue());
-		}
+		/*
+		 * String sentense=
+		 * "Hello there my name is kiran kumar kiran is having 4 years of experience java is using by many in the world these days"
+		 * ;
+		 * 
+		 * String strings[]=sentense.split(" ");
+		 * 
+		 * Map<String, Integer> resultMap = new HashMap<String, Integer>();
+		 * 
+		 * for (String str : strings) { if (resultMap.containsKey(str)) {
+		 * resultMap.put(str, resultMap.get(str) + 1); } else { resultMap.put(str, 1); }
+		 * }
+		 * 
+		 * for (Map.Entry<String, Integer> entry : resultMap.entrySet()) {
+		 * System.out.println(entry.getKey() + " = " + entry.getValue()); }
+		 */
 	}
 
 }
--- src/Test2.java
+++ src/Test2.java
@@ -1,21 +1,28 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Test2 {
 
 	public static void main(String[] args) {
-		
-		  System.out.println("Enter first number:"); Scanner scn = new
-		  Scanner(System.in); int n1 = scn.nextInt();
-		  
-		  System.out.println("Enter second number:"); int n2 = scn.nextInt();
-		  
-		  int n3=n1+n2; System.out.println(n3);
-		 
-		
-		Random rnd=new Random();
-		int x=rnd.nextInt(5);
+
+		System.out.println("Enter first number:");
+		Scanner scn = new Scanner(System.in);
+		int n1 = scn.nextInt();
+
+		System.out.println("Enter second number:");
+		int n2 = scn.nextInt();
+
+		int n3 = n1 + n2;
+		System.out.println(n3);
+
+		Random rnd = new Random();
+		int x = rnd.nextInt(5);
 		System.out.println(x);
+
 	}
 
 }
